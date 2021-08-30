6390fdb65941f39fc06b9cd230867445c3bce827
abedurftig <dasnervtdoch@googlemail.com>
2021-08-30 21:46:30 +0200

    Remove unnecessary test container setup code

--- src/test/kotlin/github/abedurftig/MicronautDataIdExampleTest.kt
+++ src/test/kotlin/github/abedurftig/MicronautDataIdExampleTest.kt
@@ -8,18 +8,12 @@ import io.micronaut.data.jdbc.annotation.JdbcRepository
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.repository.CrudRepository
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import io.micronaut.test.support.TestPropertyProvider
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.slf4j.LoggerFactory
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
 import javax.inject.Inject
 
 @MicronautTest
-class MicronautDataIdExampleTest : DatabaseTest() {
+class MicronautDataIdExampleTest {
 
     @Inject
     private lateinit var exampleEntityRepository: ExampleEntityRepository
@@ -67,42 +61,3 @@ data class ExampleEntity(
     val id: Long = Long.MAX_VALUE,
     val name: String
 )
-
-@Testcontainers
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-abstract class DatabaseTest : TestPropertyProvider {
-
-    companion object {
-
-        private val LOG = LoggerFactory.getLogger(MicronautDataIdExampleTest::class.java)
-
-        private const val DB_USER = "example-user"
-        private const val DB_PASS = "example-pass"
-        private const val DB_NAME = "example-db"
-        private const val DB_PORT = 5432
-
-        @Container
-        val container = PostgreSQLContainer<Nothing>("postgres:12").apply {
-            withDatabaseName(DB_NAME)
-            withUsername(DB_USER)
-            withPassword(DB_PASS)
-            withExposedPorts(DB_PORT)
-        }
-    }
-
-    override fun getProperties(): MutableMap<String, String> {
-
-        val host = container.host
-        val port = container.getMappedPort(DB_PORT)
-        val url = "jdbc:postgresql://$host:$port/$DB_NAME?loggerLevel=OFF"
-
-        LOG.info("Running Postgres container at '$url'")
-
-        return mutableMapOf(
-            "datasources.default.url" to url,
-            "datasources.default.username" to DB_USER,
-            "datasources.default.password" to DB_PASS,
-            "datasources.default.driverClassName" to "org.postgresql.Driver",
-        )
-    }
-}
